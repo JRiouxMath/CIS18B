53e57b0cfdd08fa60b7a43464acc4b31add48598
JRiouxMath <jeaninerioux@yahoo.com>
2018-06-02 15:45:34 -0700

    Update InventoryDatabase.java

--- Rioux_Jeanine-inventory-application/src/rioux_jeanine/inventory/application/InventoryDatabase.java
+++ Rioux_Jeanine-inventory-application/src/rioux_jeanine/inventory/application/InventoryDatabase.java
@@ -9,12 +9,15 @@ import java.sql.SQLException;
 
 public class InventoryDatabase {
     
-    static final String DB_URL = "jdbc:derby://localhost:1527/Inventory";
-    static final String SELECT_QUERY = "SELECT ID, ItemName, Description, Count, Sku, Cost FROM InventoryDB";
-    static final String USERNAME = "Sample";
+    static final String DB_URL = "jdbc:derby://localhost:1527/InventoryDatabase";
+    static final String USERNAME = "Inventory";
     static final String PASSWORD = "demo";
     
-    static final ResultSet resSet = null;
+    static final String SELECT_QUERY = "SELECT ID, ItemName, Description, Count, Sku, Cost FROM Inventory";
+    static final String DELETE_QUERY = "DELETE FROM Inventory WHERE ID=?";
+    static final String UPDATE_QUERY = "UPDATE Inventory SET ITEMNAME=?, DESCRIPTION=?, COUNT=?, SKU=?, COST=?, WHERE ID=?";
+    
+    ResultSet resSet = null;
     
     InventoryDatabase()
     {
@@ -22,6 +25,7 @@ public class InventoryDatabase {
         {
             Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
             PreparedStatement selectAllItems = connection.prepareStatement(SELECT_QUERY);
+            resSet = selectAllItems.executeQuery();
         }//end try
         catch (SQLException sqlException)
         {
@@ -31,12 +35,12 @@ public class InventoryDatabase {
         
     }// end constructor
     
-    public void addItem(int id, String itemname, String desc, int count, String Sku, double cost )
+    public void addItem(int id,  String itemname, String desc, int count, String Sku, double cost )
     {
         try
         {
             Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-            PreparedStatement insertItem = connection.prepareStatement("INSERT INTO Inventory (ID, ITEMNAME, DESC, COUNT, SKU, COST) VALUES (?,?,?,?,?,?)");
+            PreparedStatement insertItem = connection.prepareStatement("INSERT INTO Inventory (ID, ITEMNAME, DESCRIPTION, COUNT, SKU, COST) VALUES (?,?,?,?,?,?)");
             insertItem.setInt(1, id);
             insertItem.setString(2,itemname);
             insertItem.setString(3,desc);
@@ -52,6 +56,43 @@ public class InventoryDatabase {
         }// end catch block
     }// end method addItem
     
+    
+   public void deleteItem(int id)
+    {
+        try
+        {
+            Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            PreparedStatement deleteItem = connection.prepareStatement(DELETE_QUERY);
+            deleteItem.setInt(1, id);
+            deleteItem.executeUpdate();            
+        }
+        catch(SQLException sqlException)
+        {
+            sqlException.printStackTrace();
+            System.exit(4);
+        }
+    } // end method deleteItem
+   
+   public void updateItem(int P_id, String Name, String Description, int Count, String SKU, double Cost)
+    {
+        try
+        {
+            Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            PreparedStatement updateItem = connection.prepareStatement(UPDATE_QUERY);
+            updateItem.setInt(1, P_id);
+            updateItem.setString(2,Name);
+            updateItem.setString(3,Description);
+            updateItem.setInt(4,Count);
+            updateItem.setString(5,SKU);
+            updateItem.setDouble(6, Cost);
+            updateItem.executeUpdate();            
+        }
+        catch(SQLException sqlException)
+        {
+            sqlException.printStackTrace();
+            System.exit(3);
+        }
+    }// end method updateItem
     
     public ResultSet getResults()
     {
