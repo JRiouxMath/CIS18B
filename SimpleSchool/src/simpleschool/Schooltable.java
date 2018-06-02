a40e204760c92cae42136d45e88a06a7e906b6b4
JRiouxMath <jeaninerioux@yahoo.com>
2018-06-02 15:16:31 -0700

    Update Schooltable.java
    This one works, with help from Prof. Conrad.

--- SimpleSchool/src/simpleschool/Schooltable.java
+++ SimpleSchool/src/simpleschool/Schooltable.java
@@ -1,4 +1,9 @@
-package SimpleSchool;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trysimpleschool;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,22 +15,34 @@ import java.sql.SQLException;
  *
  * @author paulconrad
  */
-public class Schooltable {
+public class School_StudentTable {
 
-    static final String DB_URL = "jdbc:derby://localhost:1527/School";
-    static final String USERNAME = "school";
+    static final String DB_URL = "jdbc:derby://localhost:1527/SimpleSchool";
+    static final String USERNAME = "School";
     static final String PASSWORD = "demo";
-    static final String deleteQuery = "DELETE FROM Student WHERE STUDENTID=?";
-    static final String updateQuery =  "UPDATE Student SET STUDENTID=?, LASTNAME=?, FIRSTNAME=?, EMAIL=?, PHONENUMBER=? WHERE ID=?";
+    
+    static final String selectQuery = "SELECT * FROM Student";
+    static final String updateQuery = "UPDATE Student SET STUDENTID=?, LASTNAME=?, FIRSTNAME=?, EMAIL=?, PHONENUMBER=? WHERE ID=?";
+    static final String deleteQuery = "DELETE FROM Student WHERE ID=?";
+    
     private static ResultSet _rs=null;
 
-    Schooltable()
+    private int max_st_char, max_lname_char, max_fname_char;
+    private int max_email_char, max_phone_char;
+    
+    School_StudentTable()
     {
         try 
         {
             Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-            PreparedStatement selectAllStudents = connection.prepareStatement("SELECT * FROM Student");
+            PreparedStatement selectAllStudents = connection.prepareStatement(selectQuery);
             _rs = selectAllStudents.executeQuery();
+            
+            max_st_char = _rs.getMetaData().getPrecision(2);
+            max_fname_char = _rs.getMetaData().getPrecision(3);
+            max_lname_char = _rs.getMetaData().getPrecision(4);
+            max_email_char = _rs.getMetaData().getPrecision(5);
+            max_phone_char = _rs.getMetaData().getPrecision(6);
         }
         catch (SQLException sqlException)
         {
@@ -54,7 +71,7 @@ public class Schooltable {
         }
     }
     
-    public void updateStudent(int st_id, String st_lname, String st_fname, String st_email, String st_phone, int id)
+    public void updateStudent(int id, int st_id, String st_lname, String st_fname, String st_email, String st_phone)
     {
         try
         {
@@ -94,6 +111,6 @@ public class Schooltable {
     public ResultSet getResults()
     {
         if (_rs != null) return _rs;
-        return new Schooltable()._rs;
+        return new School_StudentTable()._rs;
     }
 }
